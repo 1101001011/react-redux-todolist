b7ffa8d166cb1573857cb3463edd372d63d43324
Raye <kerkos57@gmail.com>
2022-05-06 18:48:47 +0300

    New styles added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import TodoList from "./components/TodoList";
 import './styles/App.scss'
+import TodoForm from "./components/TodoForm";
 
 const App = () => {
     return (
         <div className="App">
+            <h1 className="App__title">TodoList</h1>
+            <TodoForm/>
             <TodoList/>
         </div>
     );
---
+++ src/components/UI/loader/MyLoader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './MyLoader.scss'
+
+const MyLoader = () => (
+    <div className="lds-ripple">
+        <div>
+        </div>
+        <div>
+        </div>
+    </div>
+)
+
+export default MyLoader;
